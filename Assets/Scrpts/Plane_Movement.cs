eb4d59f5c75a6f7efdf527ff056f8942f24fffeb
joacoorqueda <101809787+joacoorqueda@users.noreply.github.com>
2022-08-16 12:02:18 -0300

    jugadoe sigue mouse

--- Assets/Scrpts/Plane_Movement.cs
+++ Assets/Scrpts/Plane_Movement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Plane_Movement : MonoBehaviour
 {
+   
     public float MovementSpeed;
     public float RotationSpeed;
     int influence = 1;
@@ -15,12 +16,18 @@ public class Plane_Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        
+
+
+
         RB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
 
         //if(Input.GetAxisRaw("Horizontal") != 0)
         //{
@@ -32,20 +39,20 @@ public class Plane_Movement : MonoBehaviour
         //    influence *= -1;
 
         //}
-        transform.Translate(0, 0, MovementSpeed);
+        //transform.Translate(0, 0, MovementSpeed);
 
 
-        if (Input.GetKeyDown(KeyCode.Mouse1))
-        {
-            transform.Rotate(0, 180, 0);
-        }
+        //if (Input.GetKeyDown(KeyCode.Mouse1))
+        //{
+        //    transform.Rotate(0, 180, 0);
+        //}
         
-        if (Input.GetKey(KeyCode.Space))
-        {
-            RB.AddForce(new Vector3(0, JF, 0), ForceMode.Impulse);
-        }
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //    RB.AddForce(new Vector3(0, JF, 0), ForceMode.Impulse);
+        //}
 
-        Vector3 Floor = transform.TransformDirection(Vector3.down);
+        //Vector3 Floor = transform.TransformDirection(Vector3.down);
 
     }
 
---
+++ Assets/Scrpts/Playersiguecam.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playersiguecam : MonoBehaviour
+{
+    Camera cam;
+    Collider planecollider;
+    RaycastHit hit;
+    Ray ray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+        planecollider = GameObject.Find("Plane").GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5));
+        if (Input.GetMouseButton(0))
+        {
+
+
+
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider == planecollider)
+                    transform.position = Vector3.MoveTowards(transform.position, hit.point, Time.deltaTime * 5);
+                transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+            }
+
+        }
+    }
+}
