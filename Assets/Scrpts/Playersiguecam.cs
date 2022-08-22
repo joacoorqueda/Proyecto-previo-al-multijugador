12d8db96d07c6e9238e8002f2784756aeec55287
joacoorqueda <101809787+joacoorqueda@users.noreply.github.com>
2022-08-22 09:02:39 -0300

    jugador sigue mouse

--- Assets/Scrpts/Playersiguecam.cs
+++ Assets/Scrpts/Playersiguecam.cs
@@ -9,30 +9,38 @@ public class Playersiguecam : MonoBehaviour
     RaycastHit hit;
     Ray ray;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         cam = GameObject.Find("Main Camera").GetComponent<Camera>();
         planecollider = GameObject.Find("Plane").GetComponent<Collider>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        //transform.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5));
-        if (Input.GetMouseButton(0))
+        // transform.position = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5));
+
+        if (Input.GetMouseButton(1))
         {
 
+        
+        ray = cam.ScreenPointToRay(Input.mousePosition);
 
+        if (Physics.Raycast(ray, out hit))
+        {
 
-            ray = cam.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit))
+            if (hit.collider != planecollider)
             {
-                if (hit.collider == planecollider)
-                    transform.position = Vector3.MoveTowards(transform.position, hit.point, Time.deltaTime * 5);
+
+
+                transform.position = Vector3.MoveTowards(transform.position, hit.point, Time.deltaTime * 5);
                 transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
             }
 
         }
+
+
     }
+}
 }
