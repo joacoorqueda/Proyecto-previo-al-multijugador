676ea0f8f47503a824fcc7c1eb2f39e1bb785186
joacoorqueda <101809787+joacoorqueda@users.noreply.github.com>
2022-09-14 12:07:34 -0300

    pelotasmediocrementeinstanciadas

--- Assets/Scrpts/Instanciarpelotitas.cs
+++ Assets/Scrpts/Instanciarpelotitas.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class Instanciarpelotitas : MonoBehaviour
 {
     public GameObject TokenP;
+
+    private float rangoGeneracion = 9.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        float posXGeneracion = Random.Range(rangoGeneracion, rangoGeneracion);
+
+        float posZGeneracion = Random.Range(rangoGeneracion, rangoGeneracion);
+
+        Vector3 posAleatoria = new Vector3(posXGeneracion, 1, posZGeneracion);
+
+        Instantiate(TokenP, posAleatoria, TokenP.transform.rotation);
     }
 
     // Update is called once per frame
