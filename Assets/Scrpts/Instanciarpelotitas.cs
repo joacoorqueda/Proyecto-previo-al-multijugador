277aac6ea24b96c84eee429302af43050fe17530
joacoorqueda <101809787+joacoorqueda@users.noreply.github.com>
2022-09-14 10:06:51 -0300

    instanciaciondeobjetos

---
+++ Assets/Scrpts/Instanciarpelotitas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instanciarpelotitas : MonoBehaviour
+{
+    public GameObject TokenP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
