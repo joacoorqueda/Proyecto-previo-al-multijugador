c9b5bc5af19045d2a97c43a4302618b9c4694c14
joacoorqueda <101809787+joacoorqueda@users.noreply.github.com>
2022-09-15 13:50:00 -0300

    pollito de huevo

--- Assets/Scrpts/Instanciarpelotitas.cs
+++ Assets/Scrpts/Instanciarpelotitas.cs
@@ -6,7 +6,7 @@ public class Instanciarpelotitas : MonoBehaviour
 {
     public GameObject Tokenp;
 
-    private float rangoGeneracion = 3.0f;
+    private float rangoGeneracion = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +16,9 @@ public class Instanciarpelotitas : MonoBehaviour
 
         float posZGeneracion = Random.Range(rangoGeneracion, rangoGeneracion);
 
-        Vector3 posAleatoria = new Vector3(posXGeneracion, 1, posZGeneracion);
+        Vector3 posAleatoria = new Vector3(posXGeneracion, 0.85f, posZGeneracion);
+
+
 
         Instantiate(Tokenp, posAleatoria, Tokenp.transform.rotation);
     }
