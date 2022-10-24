9dcb2c55d8277541d02ac3e1ed88182037626e1f
IgnacioPagella <nacho.pagella@gmail.com>
2022-10-24 09:28:32 -0300

    ,

---
+++ Assets/Scripts Multijugador/LogicadeRed.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LogicadeRed : MonoBehaviourPunCallbacks
+{
+    public static LogicadeRed instancia;
+
+    void Awake()
+    {
+        instancia = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Felicidades estas conectado");
+    }
+}
